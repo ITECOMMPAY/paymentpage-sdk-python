d8dee5148d01816d4e189554ad7d8e40f06c3401
Vladimir Germanov <v.germanov@it.ecommpay.com>
2025-09-29 09:54:48 +0300

    PE-943: replace the card_operation_type parameter with operation_type

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -10,11 +10,13 @@ class PaymentTest(unittest.TestCase):
             'project_id': '1',
             'best_before': '2055-05-05T00:00:00',
             'interface_type': '{"id": 24}',
-            'payment_id': 'test-payment'
+            'payment_id': 'test-payment',
+            'operation_type': 'auth',
         }
 
     def test_get_params(self):
         payment = Payment('1')
         payment.payment_id = 'test-payment'
         payment.best_before = datetime(2055, 5, 5)
+        payment.card_operation_type = 'auth'
         self.assertEqual(self.compare_data, payment.get_params())
